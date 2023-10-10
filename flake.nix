aa312313d67d9cebb86dc8741cfaa9fcaa25efe7
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2023-10-10 22:27:57 +0200

    Create flake with rust dev tools

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Timetable optimizer helps you find the best course arrangement";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils"; # provides eachDefaultSystem
+    rust-overlay.url = "github:oxalica/rust-overlay"; # provides common rust packages
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [
+          # makes a `rust-bin` attribute available in nixpkgs
+          (import inputs.rust-overlay)
+        ];
+        pkgs = import inputs.nixpkgs { inherit system overlays; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # cargo, clippy, cargo-fmt, rustdoc, rustfmt, and other tools
+            rust-bin.stable.latest.default
+            cargo-watch
+          ];
+        };
+      }
+    );
+}
