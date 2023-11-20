0dc88ca9c7644548e5e0e1f6405d77f5320e901b
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2023-11-20 12:37:49 +0100

    Refactor data structures

--- src/data.rs
+++ src/data.rs
@@ -1,38 +1,58 @@
 use chrono::NaiveTime;
 
+#[derive(Debug)]
 pub struct Subject<'a> {
   pub name: &'a str,
   pub code: &'a str,
   pub credits: u32,
-  pub courses: Vec<OneOfCourse>,
+  pub courses: Vec<OneOfCourse<'a>>,
 }
 
-type OneOfCourse = Vec<Course<'static>>;
+type OneOfCourse<'a> = Vec<Course<'a>>;
+
+#[derive(Debug)]
 pub struct Course<'a> {
   pub code: &'a str,
   pub course_type: CourseType,
-  pub people_joined: u32,
-  pub people_queue: u32,
-  pub people_limit: u32,
-  pub start_time: WeekTime,
-  pub end_time: WeekTime,
+  pub enrollment: Enrollment,
   pub location: &'a str,
   pub teacher: &'a str,
-  pub language : &'a str,
+  pub language: &'a str,
   pub occurence: Occurence,
 }
 
+#[derive(Debug)]
 pub enum CourseType {
   Lecture,
   Laboratory,
   Practice,
 }
 
-pub struct WeekTime {
-  pub week_day: WeekDay,
-  pub time: NaiveTime,
+#[derive(Debug)]
+pub struct Enrollment {
+  pub people_joined: u32,
+  pub people_queue: u32,
+  pub people_limit: u32,
 }
 
+#[derive(Debug)]
+pub struct Occurence {
+  pub weeks: Weeks,
+  pub week_day: WeekDay,
+  pub start_time: NaiveTime,
+  pub end_time: NaiveTime,
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub enum Weeks {
+  Every,
+  Even,
+  Odd,
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
 pub enum WeekDay {
   Monday = 0,
   Tuesday = 1,
@@ -41,10 +61,4 @@ pub enum WeekDay {
   Friday = 4,
   Saturday = 5,
   Sunday = 6,
-}
-
-pub enum Occurence {
-  EveryWeeks,
-  EvenWeeks,
-  OddWeeks,
 }
