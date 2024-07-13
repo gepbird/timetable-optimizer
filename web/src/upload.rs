012ebac308d4879a50d6ffa4f5b35711bd4088a9
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-07-13 13:46:30 +0200

    refactor(web/upload): parse subject

--- web/src/upload.rs
+++ web/src/upload.rs
@@ -1,17 +1,19 @@
-use std::rc::Rc;
+use std::{io::Cursor, rc::Rc};
 
+use calamine::Xlsx;
+use timetable_optimizer_lib::{data::Subject, excel_parser};
 use web_sys::HtmlInputElement;
 use yew::prelude::*;
 
 #[derive(Properties, PartialEq)]
 pub struct Props {
-  pub on_files_processed: Callback<Vec<Vec<u8>>>,
+  pub on_files_processed: Callback<Vec<Subject>>,
 }
 
 #[function_component(UploadComponent)]
 pub fn upload_component(props: &Props) -> Html {
   let readers = use_state(Vec::new);
-  let processed_files: UseStateHandle<Vec<Vec<u8>>> = use_state(Vec::new);
+  let processed_files = use_state(Vec::new);
   let queued_files = use_state(Vec::new);
 
   let on_file_change = {
@@ -44,13 +46,15 @@ pub fn upload_component(props: &Props) -> Html {
         }
       }
       Some(queued_file) => {
+        let file_name = queued_file.name();
         let reader = Rc::new(gloo::file::callbacks::read_as_bytes(
           &queued_file,
           move |bytes| {
             let bytes = bytes.unwrap();
+            let subject = parse_subject(file_name, bytes);
             processed_files.set({
               let mut processed_files = (*processed_files).clone();
-              processed_files.push(bytes);
+              processed_files.push(subject);
               processed_files
             });
             queued_files.set(new_queued_files);
@@ -70,4 +74,10 @@ pub fn upload_component(props: &Props) -> Html {
       <input type="file" multiple=true onchange={on_file_change} />
     </div>
   }
+}
+
+fn parse_subject(file_name: String, bytes: Vec<u8>) -> Subject {
+  let cursor = Cursor::new(bytes);
+  let mut excel: Xlsx<_> = calamine::open_workbook_from_rs(cursor).unwrap();
+  excel_parser::parse_subject(file_name, &mut excel)
 }
